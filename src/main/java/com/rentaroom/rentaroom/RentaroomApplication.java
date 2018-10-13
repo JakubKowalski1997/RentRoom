bba4567d85d196a78a419812751dd71abe26e41f
TomaszMichalski <tomasz.michalski19@gmail.com>
2018-10-14 00:28:16 +0200

    Initial commit

---
+++ src/main/java/com/rentaroom/rentaroom/RentaroomApplication.java
@@ -0,0 +1,12 @@
+package com.rentaroom.rentaroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentaroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentaroomApplication.class, args);
+	}
+}
