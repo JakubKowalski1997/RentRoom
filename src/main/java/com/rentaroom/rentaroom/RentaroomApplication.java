0d1719711307f568d250924579befd1c9b71d061
TomaszMichalski <tomasz.michalski19@gmail.com>
2018-10-14 12:37:13 +0200

    PostgreSQL integration

--- src/main/java/com/rentaroom/rentaroom/RentaroomApplication.java
+++ src/main/java/com/rentaroom/rentaroom/RentaroomApplication.java
@@ -2,9 +2,16 @@ package com.rentaroom.rentaroom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RentaroomApplication {
+public class RentaroomApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+		return applicationBuilder.sources(RentaroomApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RentaroomApplication.class, args);
