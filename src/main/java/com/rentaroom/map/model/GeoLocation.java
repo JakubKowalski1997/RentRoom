bda69883993d66573ef4ab062326e21dd3d636d0
JakubKowalski1997 <kubabartek1@op.pl>
2018-11-01 19:40:43 +0100

    address and geolocation models

---
+++ src/main/java/com/rentaroom/map/model/GeoLocation.java
@@ -0,0 +1,39 @@
+package com.rentaroom.map.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "geo_location")
+public class GeoLocation {
+    private Long id;
+
+    private double latitude;
+
+    private double longitude;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
