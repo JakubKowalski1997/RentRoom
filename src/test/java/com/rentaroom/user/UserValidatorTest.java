0df42d76d5511178395bb027f5fa85302e6b66ea
Jakub Kowalski <kubabartek1@op.pl>
2019-03-07 19:57:37 +0100

    test fix and h2 instead of postgre

--- src/test/java/com/rentaroom/user/UserValidatorTest.java
+++ src/test/java/com/rentaroom/user/UserValidatorTest.java
@@ -1,12 +1,15 @@
-package com.rentaroom.rentaroom.user;
-
+package com.rentaroom.user;
 import com.rentaroom.security.user.model.User;
+import com.rentaroom.security.user.service.UserService;
 import com.rentaroom.security.user.validator.UserValidator;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
@@ -15,67 +18,51 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
-
-@RunWith(SpringRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 public class UserValidatorTest {
 
-    @Autowired
+    @InjectMocks
     private UserValidator userValidator;
 
-    @Test
-    public void emptyUsernameField() {
-        User invalidUser = new User();
-        invalidUser.setId(1L);
-        invalidUser.setUsername("");
-        invalidUser.setPassword("dupa123");
-        invalidUser.setPasswordConfirm("dupa123");
+    @Mock
+    private UserService userServiceMock;
 
-        Errors errors = new BeanPropertyBindingResult(invalidUser, "invalidUser");
-        userValidator.validate(invalidUser, errors);
-
-        FieldError usernameFieldError = errors.getFieldError("username");
-        assertThat("username", is(usernameFieldError.getField()));
-
-        List<String> errorCodes = Arrays.asList(usernameFieldError.getCodes());
-        assertThat(4, is(errorCodes.size()));
-        assertTrue(errorCodes.contains("NotEmpty.invalidUser.username"));
-        assertTrue(errorCodes.contains("NotEmpty.username"));
-        assertTrue(errorCodes.contains("NotEmpty.java.lang.String"));
-        assertTrue(errorCodes.contains("NotEmpty"));
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    public void usernameLengthUpperBoundaryTesting() {
-        User invalidUser = new User();
-        invalidUser.setId(1L);
-        invalidUser.setUsername("fapllllololololololololollololss2");
-        invalidUser.setPassword("faplllosss");
-        invalidUser.setPasswordConfirm("faplllosss");
+    public void userAlreadyExists() {
+        when(userServiceMock.findByUsername("faplllosss")).thenReturn(new User());
 
-        Errors errors = new BeanPropertyBindingResult(invalidUser, "invalidUser");
-        userValidator.validate(invalidUser, errors);
+        User duplicateUser = new User();
+        duplicateUser.setId(1L);
+        duplicateUser.setUsername("faplllosss");
+        duplicateUser.setPassword("faplllosss123");
+        duplicateUser.setPasswordConfirm("faplllosss123");
+        duplicateUser.setFirstName("Jakub");
+        duplicateUser.setLastName("Nowak");
+
+        Errors errors = new BeanPropertyBindingResult(duplicateUser, "duplicateUser");
+        System.out.println(errors + " " + duplicateUser);
+        userValidator.validate(duplicateUser, errors);
 
         FieldError usernameFieldError = errors.getFieldError("username");
+        assertNotNull(usernameFieldError);
+        assertNotNull(usernameFieldError.getCodes());
         assertThat("username", is(usernameFieldError.getField()));
-        List<String> usernameErrorCodes = Arrays.asList(usernameFieldError.getCodes());
-        assertThat(4, is(usernameErrorCodes.size()));
-
-        assertTrue(usernameErrorCodes.contains("Size.userForm.username.invalidUser.username"));
-        assertTrue(usernameErrorCodes.contains("Size.userForm.username"));
-        assertTrue(usernameErrorCodes.contains("Size.userForm.username.java.lang.String"));
-        assertTrue(usernameErrorCodes.contains("Size.userForm.username"));
-
-        User validUser = new User();
-        validUser.setId(1L);
-        validUser.setUsername("fapllllololololololololollololss");
-        validUser.setPassword("faplllosss");
-        validUser.setPasswordConfirm("faplllosss");
-
-        Errors errorsValid = new BeanPropertyBindingResult(validUser, "validUser");
-        userValidator.validate(validUser, errorsValid);
-        assertNull(errorsValid.getFieldError("username"));
+        List<String> errorCodes = Arrays.asList(usernameFieldError.getCodes());
+        assertThat(4, is(errorCodes.size()));
+        assertTrue(errorCodes.contains("Duplicate.userForm.username.duplicateUser.username"));
+        assertTrue(errorCodes.contains("Duplicate.userForm.username.username"));
+        assertTrue(errorCodes.contains("Duplicate.userForm.username.java.lang.String"));
+        assertTrue(errorCodes.contains("Duplicate.userForm.username"));
     }
 }
